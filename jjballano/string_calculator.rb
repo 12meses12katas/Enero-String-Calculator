44c4acc119cd17f7d949e0ce8f52028229c9949a
Jesús Jiménez Ballano <jjballano@Jesus-Jimenez-Ballanos-MacBook-Pro.local>
2011-01-25 22:50:04 +0100

    first group of test ok

--- jjballano/string_calculator.rb
+++ jjballano/string_calculator.rb
@@ -1,3 +1,17 @@
-class StringCaculator
+class StringCalculator
 
+   def calculate (string)
+      string.nil? || string.empty? ? 0 : calculate_sum_of(string)
+   end
+
+   def calculate_sum_of(string)
+      numbers = split_string_in_numbers(string)
+      sum = 0
+      numbers.inject(0) { |number, sum| sum += number }
+      return sum
+   end
+
+   def split_string_in_numbers(string)
+      string.split(',').map{ |number| number.to_i}
+   end
 end
--- jjballano/string_calculator_test.rb
+++ jjballano/string_calculator_test.rb
@@ -2,5 +2,33 @@ require 'string_calculator'
 
 describe StringCalculator do
 
-end
+   before(:each) do
+      @calculator = StringCalculator.new
+   end
 
+   describe "empty_string_return_0" do
+      it "returns 0 if the string is empty" do
+         @calculator.calculate(nil).should == 0
+         @calculator.calculate("").should == 0
+      end
+   end
+
+  describe "string_with_one_number_return_that_number" do
+      it "returns 5 if the string is '5'" do
+	 @calculator.calculate("5").should == 5
+      end
+  end
+  
+  describe "string_with_two_number_return_a_sum_of_those_numbers" do
+      it "returns 12 if the string is '7,5'" do
+	 @calculator.calculate("7,5").should == 12 
+      end
+  end
+
+  describe "string_with_few_numbers_return_a_sum_of_those_numbers" do
+      it "returns 24 if string is '3,5,10,6'" do
+	 @calculator.calculate("3,5,10,6").should == 24
+      end
+  end
+ 
+end
