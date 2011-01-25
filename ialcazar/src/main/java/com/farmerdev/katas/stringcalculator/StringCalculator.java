e300cb0c587eafcb1f4c5e213760e81a571247b5
ialcazar <israelalcazar@gmail.com>
2011-01-25 23:50:43 +0100

    Added test two numbers are taken

--- ialcazar/src/main/java/com/farmerdev/katas/stringcalculator/StringCalculator.java
+++ ialcazar/src/main/java/com/farmerdev/katas/stringcalculator/StringCalculator.java
@@ -3,10 +3,20 @@ package com.farmerdev.katas.stringcalculator;
 public class StringCalculator {
 
 	public int add(String stringToParse) {
+		int result = 0;
 		if("".equals(stringToParse))
-			return 0;
-		else
-			return Integer.parseInt(stringToParse);
+			result = 0;
+		else if(stringToParse.contains(",")){
+			result = 0;
+			String [] numbersToAdd = stringToParse.split(",");
+			for(String aux:numbersToAdd){
+				result += Integer.parseInt(aux);
+			}
+			
+		}else
+			result = Integer.parseInt(stringToParse);
+		
+		return result;
 	}
 
 }
