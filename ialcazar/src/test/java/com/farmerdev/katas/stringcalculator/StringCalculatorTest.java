42db8cc84737bbf365a17db8eab57ad41e4e1476
ialcazar <israelalcazar@gmail.com>
2011-01-26 22:43:00 +0800

    Completed implementation until section 5

--- ialcazar/src/test/java/com/farmerdev/katas/stringcalculator/StringCalculatorTest.java
+++ ialcazar/src/test/java/com/farmerdev/katas/stringcalculator/StringCalculatorTest.java
@@ -6,7 +6,10 @@ import static org.junit.Assert.assertThat;
 
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
+
+import com.farmerdev.katas.stringcalculator.exceptions.NumberException;
 
 public class StringCalculatorTest {
 	private static StringCalculator stringCalculator = null;
@@ -56,5 +59,60 @@ public class StringCalculatorTest {
 			assertThat(result, is(resultExpected[i]));
 		}
 	}
+	
+	@Test
+	public void when_new_line_delimiter_is_added_then_should_return_their_sum(){
+		String [] valuesTaken={"1\n2","2\n10,22","10,100,9\n1,4,23","20,100,500\n2"};
+		int [] resultExpected ={3,34,147,622};
+		for(int i=0;i<valuesTaken.length;i++){		
+			int result = stringCalculator.add(valuesTaken[i]);
+			assertThat(result, is(resultExpected[i]));
+		}
+	}
+	
+	@Test
+	public void when_new_delimiter_is_supported_then_should_take_as_new_delimiter_and_sum_the_numbers(){
+		String [] valuesTaken={"//;\n2;3","//+\n10+22","//-\n10-100-9-1-4-23"};
+		int [] resultExpected ={5,32,147};
+		for(int i=0;i<valuesTaken.length;i++){		
+			int result = stringCalculator.add(valuesTaken[i]);
+			assertThat(result, is(resultExpected[i]));
+		}
+	}
+	@Test
+	public void when_new_delimiter_is_supported_without_new_line_then_should_take_as_new_delimiter_and_sum_the_numbers(){
+		String [] valuesTaken={"//;2;3","//+10+22","//-10-100-9-1-4-23"};
+		int [] resultExpected ={5,32,147};
+		for(int i=0;i<valuesTaken.length;i++){		
+			int result = stringCalculator.add(valuesTaken[i]);
+			assertThat(result, is(resultExpected[i]));
+		}
+	}
+	
+	@Test(expected=com.farmerdev.katas.stringcalculator.exceptions.NumberException.class)
+	public void when_a_negative_number_is_added_then_should_throw_an_exception(){
+		
+		stringCalculator.add("2,-3");
+		
+	}
+	
+	@Test
+	public void when_a_negative_number_is_added_then_should_throw_an_exception_with_a_message(){
+		String message = "Negative numbers are not allowed [-3 -5 ]";
+		try{
+			
+			stringCalculator.add("2,-3,-5");
+			fail("Must throw an Exception");
+			
+		}catch(NumberException ex){
+			assertThat(message,is(ex.getMessage()));
+		}
+	}
+	
+	
+	
+	
+	
+	
 	
 }
