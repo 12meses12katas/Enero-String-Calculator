5f9bf60d0f24da32444a9b21addbd5d764f28f68
Rafa de Castro <rafael@micubiculo.com>
2011-01-29 06:46:04 +0800

    Added String Calculator specs and made it able to be run from Eclipse as a jUnit test

---
+++ rafadc/src/micubiculo/StringCalculatorSpec.scala
@@ -0,0 +1,10 @@
+package micubiculo
+
+import org.junit.runner.RunWith
+import org.specs.runner.JUnitSuiteRunner
+import org.specs.SpecificationWithJUnit
+
+@RunWith(classOf[JUnitSuiteRunner])
+class StringCalculatorSpec extends SpecificationWithJUnit {
+
+}
