4f088dddb69dedfd2be994b96ffab1c7633440ad
Miguel Angel Garcia <miguelangel.garcia@gmail.com>
2011-01-29 10:44:28 +0100

    green. Vuelvo a añadirlo y refactorizo

--- magmax/python/stringcalculator.py
+++ magmax/python/stringcalculator.py
@@ -4,20 +4,25 @@
 import re
 
 class StringCalculator:
+    def __init__ (self):
+        self.input = None
+        self.separator = ','
+
     def add(self, number):
         if not number:
             return 0
+        self.input = number
         result = 0
-        for val in self.__split_string(number):
+        for val in self.__split_string():
             result += int(val)
         return result
 
-    def __split_string (self, string):
-        self.__validate(string, ',')
-        return re.findall('(\d+)[,|\n]?', string)
+    def __split_string (self):
+        self.__validate()
+        return re.findall('(\d+)[,|\n]?', self.input)
 
-    def __validate(self, string, separator):
-        pattern = re.compile('^(//.\n)?(\d+[\n|%s])*(\d+)$'%separator)
-        if not re.search(pattern, string):
+    def __validate(self):
+        pattern = re.compile('^(//.\n)?(\d+[\n|%s])*(\d+)$'%self.separator)
+        if not re.search(pattern, self.input):
             raise SyntaxError("invalid input")
     
