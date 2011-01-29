1fd558063d0154f46b4766bcfbe447b520ef09db
Miguel Angel Garcia <miguelangel.garcia@gmail.com>
2011-01-29 16:54:45 +0800

    Refactorizando

--- magmax/python/stringcalculatortest.py
+++ magmax/python/stringcalculatortest.py
@@ -5,17 +5,17 @@ import unittest
 from stringcalculator import StringCalculator
 
 class StringCalculatorTest (unittest.TestCase):
+    def setUp(self):
+        self.sut = StringCalculator()
+
     def test_empty_string(self):
-        sut = StringCalculator()
-        self.assertEqual(0, sut.add(""))
+        self.assertEqual(0, self.sut.add(""))
 
     def test_string_with_a_number(self):
-        sut = StringCalculator()
-        self.assertEqual(1, sut.add("1"))
+        self.assertEqual(1, self.sut.add("1"))
 
     def test_string_with_a_long_number(self):
-        sut = StringCalculator()
-        self.assertEqual(123456789, sut.add("123456789"))
+        self.assertEqual(123456789, self.sut.add("123456789"))
 
 if __name__ == '__main__':
     unittest.main()
