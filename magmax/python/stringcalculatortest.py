be6e2c932c0088ec9d635a8ae5e220817ffe6785
Miguel Angel Garcia <miguelangel.garcia@gmail.com>
2011-01-29 10:37:20 +0100

    green. Validaciones básicas

--- magmax/python/stringcalculatortest.py
+++ magmax/python/stringcalculatortest.py
@@ -29,7 +29,16 @@ class StringCalculatorTest (unittest.TestCase):
     def test_return_is_a_separator(self):
         self.assertEqual(9, self.sut.add("1,2\n6"))
 
-    def test_changing_separator(self):
+    def test_invalid_input_ends_with_separator(self):
+        self.assertRaises(SyntaxError, self.sut.add, "1,2,")
+
+    def test_invalid_input_without_separator(self):
+        self.assertRaises(SyntaxError, self.sut.add, "//\n1")
+    
+    def _test_invalid_input_erroneous_separator(self):
+        self.assertRaises(SyntaxError, self.sut.add, "//;\n1,2\n3")
+
+    def _test_changing_separator(self):
         self.assertEqual(3, self.sut.add("//;\n1;2"))
 
 if __name__ == '__main__':
