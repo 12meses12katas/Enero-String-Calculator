fbe25260ad69323881e8c350c8447943e38c8cf7
Miguel Angel Garcia <miguelangel.garcia@gmail.com>
2011-01-29 16:43:32 +0800

    Estructura inicial string calculator en python

---
+++ magmax/python/stringcalculatortest.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import unittest
+
+class StringCalculatorTest (unittest.TestCase):
+    def test_fails(self):
+        self.fail()
+
+if __name__ == '__main__':
+    unittest.main()
