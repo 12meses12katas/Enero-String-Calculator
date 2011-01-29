b9ec8bcb0fedaded89af2821c9af500604502bfc
francho <francho@192.168.33.10>
2011-01-29 15:28:11 +0100

    Mi solución en java basada en expresiones regulares.
    Las pruebas estan realizadas con JUnit4

---
+++ francho_lab/src/org/francho/katas/enero/NegativesNotAllowedException.java
@@ -0,0 +1,29 @@
+/**
+ * Ejercicio de 12meses12katas.com
+ * 
+ * by francho
+ * 
+ * Twitter: @francho_lab
+ * http://francho.org  
+ */
+package org.francho.katas.enero;
+
+/**
+ * @author francho
+ *
+ */
+public class NegativesNotAllowedException extends IllegalArgumentException {
+
+	/**
+	 * @param bad
+	 */
+	public NegativesNotAllowedException(String error) {
+		super(error);
+	}
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -8151113504458717163L;
+
+}
