32967ad6a361bc49e0afb6871affe95ad742aa4c
Rafa de Castro <rafael@micubiculo.com>
2011-01-29 08:11:33 +0800

    Added calculations for string only with a number

--- rafadc/src/micubiculo/StringCalculatorSpec.scala
+++ rafadc/src/micubiculo/StringCalculatorSpec.scala
@@ -7,4 +7,26 @@ import org.specs.SpecificationWithJUnit
 @RunWith(classOf[JUnitSuiteRunner])
 class StringCalculatorSpec extends SpecificationWithJUnit {
 
+  "String calculator" should {
+    "return 0 in case the input is an empty string" in {
+      StringCalculator.add("") must beEqualTo(0)
+    }
+
+    "return 3 in case the input '3'" in {
+      StringCalculator.add("3") must beEqualTo(3)
+    }
+
+    "return 12 in case the input '12'" in {
+      StringCalculator.add("12") must beEqualTo(12)
+    }
+  }
+}
+
+object StringCalculator {
+  def add(input: String): Integer = {
+    if (isEmpty(input)) return 0
+    else return input.toInt
+  }
+
+  def isEmpty(input: String): Boolean = input.size == 0
 }
