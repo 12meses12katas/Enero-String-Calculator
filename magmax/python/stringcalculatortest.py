6f194decd5475cf19eb58ab999a43a9d7d14ddaa
Miguel Angel Garcia <miguelangel.garcia@gmail.com>
2011-01-29 16:48:29 +0800

    working for empty string

--- magmax/python/stringcalculatortest.py
+++ magmax/python/stringcalculatortest.py
@@ -2,10 +2,12 @@
 # -*- coding: utf-8 -*-
 
 import unittest
+from stringcalculator import StringCalculator
 
 class StringCalculatorTest (unittest.TestCase):
-    def test_fails(self):
-        self.fail()
+    def test_empty_string(self):
+        sut = StringCalculator()
+        self.assertEqual(0, sut.add(""))
 
 if __name__ == '__main__':
     unittest.main()
