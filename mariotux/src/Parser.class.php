0e76369cc03668f92214435d88474890d1e2d8fe
mariotux <mariotux@laptop.(none)>
2011-01-30 04:53:00 +0800

    Simple refactoring

--- mariotux/src/Parser.class.php
+++ mariotux/src/Parser.class.php
@@ -1,11 +1,8 @@
 <?php
 
 class Parser {
-    public function stringNumbers($str_input) {
-        return $this->getArrayNumbers($str_input);
-    }
-    
-    private function getArrayNumbers($str){
+  
+    public function parseNumbersSequence($str){
         $delimiters = array_unique(preg_split("/-?\d/", $str, null, PREG_SPLIT_NO_EMPTY));
         foreach($delimiters as $delimiter){
             $str = str_replace($delimiter,',',$str);
