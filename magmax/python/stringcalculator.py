80064a15e84d03d70b3b5b4f6b7bf8b284d435f7
Miguel Angel Garcia <miguelangel.garcia@gmail.com>
2011-01-29 10:10:56 +0100

    return es un separador

--- magmax/python/stringcalculator.py
+++ magmax/python/stringcalculator.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import re
+
 class StringCalculator:
-    def split_string (self, string):
-        return string.split(',')
+    def __split_string (self, string):
+        return re.findall('(\d+)[,|\n]?', string)
 
     def add(self, number):
         if not number:
             return 0
         result = 0
-        for val in self.split_string(number):
+        for val in self.__split_string(number):
             result += int(val)
         return result
 
