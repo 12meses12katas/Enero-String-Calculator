b6d170a4ed327a0a77c7d64a5d87eedec6d873c0
Rafa de Castro <rafael@micubiculo.com>
2011-01-30 13:51:09 +0100

    Added \n as a separator

--- rafadc/src/micubiculo/StringCalculator.scala
+++ rafadc/src/micubiculo/StringCalculator.scala
@@ -2,8 +2,11 @@ package micubiculo
 
 object StringCalculator {
   def add(input: String): Integer = {
+	val separators = new Array[Char](2)
+	separators(0) = ','
+	separators(1) = '\n'
     if (isEmpty(input)) return 0
-    else return input.split(",").foldLeft(0) {(sum, current) => sum + current.toInt }
+    else return input.split(separators).foldLeft(0) {(sum, current) => sum + current.toInt }
   }
 
   private def isEmpty(input: String): Boolean = input.size == 0
