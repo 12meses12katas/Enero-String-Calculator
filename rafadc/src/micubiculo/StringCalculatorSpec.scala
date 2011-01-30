b9c71d7305f0d37299e4834832e3bccfa2c21fd1
Rafa de Castro <rafael@micubiculo.com>
2011-01-30 17:54:05 +0100

    Added exception in case negatives are thrown

--- rafadc/src/micubiculo/StringCalculatorSpec.scala
+++ rafadc/src/micubiculo/StringCalculatorSpec.scala
@@ -19,29 +19,37 @@ class StringCalculatorSpec extends SpecificationWithJUnit {
     "return 12 in case the input '12'" in {
       StringCalculator.add("12") must beEqualTo(12)
     }
-    
+
     "return the sum in case two number are given" in {
       StringCalculator.add("12,5") must beEqualTo(17)
     }
-    
+
     "return the sum in case four number are given" in {
       StringCalculator.add("12,5,1,1") must beEqualTo(19)
     }
-    
+
     "return the sum in case two number are given separated with a \n" in {
       StringCalculator.add("12\n5") must beEqualTo(17)
     }
-    
+
     "return the sum in case we use different separators" in {
       StringCalculator.add("12,1\n5") must beEqualTo(18)
     }
-    
+
     "the default delimiter is changed when first line starts with //<delimiter>" in {
       StringCalculator.add("//;\n3;5") must beEqualTo(8)
     }
-    
+
     "the default delimiter is changed and we can mix with \n" in {
       StringCalculator.add("//;\n3;5\n2") must beEqualTo(10)
+    }
+
+    "an exception must be thrown when negative number is passed" in {
+      StringCalculator.add("-1") must throwA(new RuntimeException("negative numbers not allowed: -1"))
+    }
+
+    "an exception must be thrown when negative number is passed" in {
+      StringCalculator.add("-3") must throwA(new RuntimeException("negative numbers not allowed: -3"))
     }
   }
 }
