b45b30fd36433d5c90f31c623f6c50626aa423ba
fperezpt <fperez@palmatools.com>
2011-01-30 18:54:03 +0800

    Free Pascal (Lazarus IDE): Without regexp.
    Inyecting the parser & validator in StringCalculator.
    Complete testing suite over Parser/Validator/StringCalculator.

--- fperezpt/StringCalculator/ustringcalculator.pas
+++ fperezpt/StringCalculator/ustringcalculator.pas
@@ -5,26 +5,18 @@ unit uStringCalculator;
 interface
 
 uses
-  Classes, SysUtils, fgl;
+  Classes, SysUtils, uHelperCalculator;
 
 type
-  TIntegerList = specialize TFPGList<Integer>;
-  ExceptionNegativeAddend = Class(Exception);
-  ExceptionDelimiterFormat = Class(Exception);
-
   { TStringCalculator }
   TStringCalculator = class
   private
-    FDelimiterInitToken: string;
-    FDefaultDelimiter: char;
-    procedure SetValidDelimiters(var Addends: string);
-    function SplitAddends(Addends: string): TStringList;
-    function CheckAddendsValues(const Addends: TStringList): TIntegerList;
-    property DelimiterInitToken: string read FDelimiterInitToken;
+    FParser: IParserNumbers;
+    FValidator: IValidation;
+    function AddListNumbers(NumbersList: TIntegerList): Integer;
   public
-    constructor Create(DefaultDelimiter: char=',');
-    function Add(const Addends: string): Integer;
-    property DefaultDelimiter: char read FDefaultDelimiter;
+    constructor Create(Parser: IParserNumbers; Validator: IValidation);
+    function Add(const Numbers: string): Integer;
   end;
 
 
@@ -32,83 +24,25 @@ implementation
 
 { TStringCalculator }
 
-procedure TStringCalculator.SetValidDelimiters(var Addends: string);
+function TStringCalculator.AddListNumbers(NumbersList: TIntegerList): Integer;
 var
-  longInitToken: Byte;
+  Number: Integer;
 begin
-  longInitToken := Length(DelimiterInitToken);
-  if (copy(Addends, 1, longInitToken) = DelimiterInitToken) then
-  begin
-    if (copy(Addends, longInitToken + 2 , 2) = '\n') then
-    begin
-      // Get default token
-      FDefaultDelimiter := Addends[longInitToken + 1];
-      // Remove init token + delimiter (1) + \n separator (2).
-      Addends := copy(Addends, longInitToken + 1 + 2 + 1, length(Addends));
-    end
-    else
-    begin
-      raise ExceptionDelimiterFormat.Create('Delimiter format error');
-    end;
-  end;
-  // '\n' is not a new line in Pascal.  It must be replaced as any other string.
-  Addends := StringReplace(Addends, '\n', DefaultDelimiter, [rfReplaceAll] );
-end;
-
-function TStringCalculator.SplitAddends(Addends: string): TStringList;
-begin
-  SetValidDelimiters(Addends);
-  result := TStringList.Create;
-  with result do
-  begin
-    Clear;
-    Delimiter:= DefaultDelimiter;
-    StrictDelimiter := true;
-    DelimitedText:= Addends;
-  end;
-end;
-
-function TStringCalculator.CheckAddendsValues(const Addends: TStringList): TIntegerList;
-var
-  x: Integer;
-begin
-  result := TIntegerList.Create;
-  for x := 0 to Addends.Count - 1 do
-  begin
-    result.Add( StrToIntDef( Addends.Strings[x], 0) );
-    if (result.Get(result.Count-1) < 0) then
-      raise ExceptionNegativeAddend.Create('negatives not allowed');
-  end;
-end;
-
-constructor TStringCalculator.Create(DefaultDelimiter: char);
-begin
-  if (DefaultDelimiter = '') then
-    FDefaultDelimiter := ','
-  else
-    FDefaultDelimiter := DefaultDelimiter;
-
-  FDelimiterInitToken := '//';
-end;
-
-function TStringCalculator.Add(const Addends: string): Integer;
-var
-  AddendList: TIntegerList;
-  Addend: Integer;
-begin
-  if Addends = '' then exit(0);
-
   result := 0;
+  for Number in NumbersList do
+    inc(result, Number);
+end;
 
-  AddendList := CheckAddendsValues( SplitAddends(Addends) );
-  try
-    for Addend in AddendList do
-    begin
-      inc(result, Addend);
-    end;
-  finally
-    FreeAndNil(AddendList);
-  end;
+constructor TStringCalculator.Create(Parser: IParserNumbers; Validator: IValidation);
+begin
+  FParser := Parser;
+  FValidator := Validator;
+end;
+
+function TStringCalculator.Add(const Numbers: string): Integer;
+begin
+  result := self.AddListNumbers(
+                      FValidator.ValidateInput( FParser.SplitNumbers(Numbers)));
 end;
 
 end.
