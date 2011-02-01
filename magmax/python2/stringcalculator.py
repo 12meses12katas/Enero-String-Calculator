bab2056e9de7b010bb6587e22fd376d8da667aa3
miguel angel garcia <miguel@nightcrawler.(none)>
2011-02-01 14:59:34 +0800

    añado validación de números negativos, refactorizo la comprobación de números grandes y permito delimitadores multicaracter

--- magmax/python2/stringcalculator.py
+++ magmax/python2/stringcalculator.py
@@ -10,33 +10,30 @@ class Elements:
         if not self.input:
             raise StopIteration
 
-        result = self.__get_token_and_jump()
-        return self.__invalidate_big_numbers(result)
-
-    def __invalidate_big_numbers(self, value):
-        result = int(value)
-        if result > 1000: 
-            return 0
-        return result
+        token = self.__get_token_and_jump()
+        return int(token)
 
     def __get_token_and_jump(self):
         length = self.__get_next_token_length()
         if not length:
             raise SyntaxError('Entrada inválida: ' + self.input )
         result = self.input[:length]
-        self.input = self.input[length+1:]
+        self.input = self.input[length+len(self.delimiter):]
         return result
 
     def __get_next_token_length(self):
         result = 0
-        for char in self.input:
-            if self.__is_delimiter(char):
+        while result < len(self.input):
+            if self.__there_is_a_separator_at(result):
                 break
             result += 1
         return result
 
-    def __is_delimiter(self, value):
-        return value in (self.delimiter, '\n')
+    def __there_is_a_separator_at(self, pos):
+        for delimiter in ('\n', self.delimiter):
+            if self.input[pos:].startswith(delimiter):
+                return True
+        return False
 
 class Separator:
     def __init__(self, value):
@@ -47,16 +44,48 @@ class Separator:
         return Elements(delimiter, value)
 
     def __separe_statements(self):
+        value = self.value
+        delimiter = ','
         if self.__has_delimiter_specification():
-            return self.value[2], self.value[4:]
-        return ',',self.value
+            value = self.__separe_values ()
+            delimiter = self.__separe_delimiter()
+        return delimiter, value
+
+    def __separe_values(self):
+        pos = self.value.index('\n')
+        return self.value[pos+1:]
+            
+    def __separe_delimiter(self):
+        result = None
+        if self.value[2] == '[':
+            end = self.value.index(']')
+            result = self.value[3:end]
+        else:
+            result = self.value[2]
+        return result
 
     def __has_delimiter_specification(self):
-        return self.value and self.value[0] == '/'
+        return len(self.value) > 2  and self.value[0] == '/' and self.value[1] == '/'
     
 class StringCalculator:
+    def __init__ (self):
+        self.negatives = []
+        self.sum = 0
+
     def add(self, number):
-        result = 0
         for value in Separator(number):
-            result += value
-        return result
+            self.__process_number(value)
+        self.__no_negatives_assertion()
+        return self.sum
+
+    def __no_negatives_assertion(self):
+        if not len(self.negatives):
+            return
+        raise ValueError ("negatives not allowed:" + str(self.negatives))
+
+    def __process_number(self, number):
+        if number < 0:
+            self.negatives.append(number)
+        elif number < 1000:
+            self.sum += number
+        
