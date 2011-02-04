fdec67c019e5c3912e610de8275a706c1a2b1d11
Eddy Josafat Hernández Vega <ejosafat@Metropolis.local>
2011-02-04 22:53:35 +0800

    Task 4 is finished

--- ejosafat/spec/string_calculator_spec.rb
+++ ejosafat/spec/string_calculator_spec.rb
@@ -1,6 +1,7 @@
 require "spec_helper"
 
 describe "add" do
+  # Task 1
   it "should return 0 when string is empty" do
     StringCalculator.add('').should == 0
   end
@@ -10,14 +11,19 @@ describe "add" do
   it "should return the sum of two numbers" do
     StringCalculator.add('3,4').should == 7
   end
+  
+  # Task 2
   it "should handle any amount of numbers" do
     StringCalculator.add('3,10,25,18').should == 56
   end
+  
+  # Task 3
   it "should handle , and \n as delimiters" do
     StringCalculator.add("3,10\n25\n18").should == 56
   end
-  # Use this test if single quote strings are allowed
-  # it "should handle , and \n as delimiters" do
-  #   StringCalculator.add('3,10\n25\n18').should == 56
-  # end
+  
+  # Task 4
+  it "should handle any delimiter" do
+    StringCalculator.add("//;\n3,10\n25;18").should == 56
+  end
 end
--- ejosafat/string_calculator.rb
+++ ejosafat/string_calculator.rb
@@ -1,8 +1,13 @@
+# Assumption: numbers is a doble quoted string with no invalid data
 class StringCalculator
   class << self
     def add( numbers )
-      # Add |\\n to the regexp if single quote strings are allowed
-      numbers.split(/\n|,/).map(&:to_i).inject(0) { |sum, n| sum + n }
+      regexp = "\n|,"
+      if numbers =~ /(\/\/(.)\n)/
+        regexp << "|#{$2}"
+        numbers.gsub! $1.to_s, ''
+      end
+      numbers.split(/#{regexp}/).map(&:to_i).inject(0) { |sum, n| sum + n }
     end
   end
 end
