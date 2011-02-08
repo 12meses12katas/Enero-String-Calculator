f7b645f50f6f09b4b4db37bd16b0889cf71f95f3
Sebastian Ortega <sortega@netleading.com>
2011-02-09 00:58:02 +0100

    sortega: step 3

--- sortega/calculator.rb
+++ sortega/calculator.rb
@@ -1,9 +1,5 @@
 def add(numbers)
   total = 0
-  numbers.split(',').each { |n| total += n.to_i }
+  numbers.split(/,|\n/).each { |n| total += n.to_i }
   return total
 end
-
-add("")
-add("1")
-add("1,2")
