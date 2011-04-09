7c20b170f34d27b5a5cfc0ef539aea371e1b7854
alex952 <alex952.anf5@gmail.com>
2011-04-09 18:58:19 +0200

    Múltiples separadores

--- alex952/stringCalculator.rb
+++ alex952/stringCalculator.rb
@@ -1,11 +1,36 @@
 class StringCalculator
 
 	def Add(param)
-		return 0 if param == ""
+		#It's a blank line of parameters
+		return 0 if param == "" 
 
-		params = param.split(',')
+		#Initilize separator to base case
+		separator = ','
+
+		#Searching for a separator field within param string
+		sep_fetch = param.split('\n')
+		puts "El array #{sep_fetch} tiene #{sep_fetch.length} longitud y #{sep_fetch[0].start_with?("//")}"
+		if sep_fetch.length > 1 && sep_fetch[0].start_with?("//") then
+			#Exists the separator within params string
+			separator = sep_fetch[0][2]
+		else 
+			#Search for separator
+			param.each_char do |c|
+				begin
+					Integer(c)
+				rescue
+					separator = c
+					break
+				end
+			end
+		end
+
+
+		#Split the parameters from a separator
+		params = param.split(separator)
 		res = 0
 
+		#Add the parts, the operands
 		params.each do |p|
 			res += Integer(p)
 		end
