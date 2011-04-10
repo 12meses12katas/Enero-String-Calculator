fb5dd2cb538ff3bd0a5b31bee003cd4168d134ba
Guillermo <erpheus@gmail.com>
2011-04-10 14:32:39 +0200

    Fixed negative numbers resolution. Now it raises an error

--- erpheus/calculator.rb
+++ erpheus/calculator.rb
@@ -28,11 +28,19 @@
       end
       divided=thistime
     end
-    
+    error=false
+    negative=Array.new
     divided.each do |d|
-      if (d.to_i>0)&&(d.to_i <= 1000)
+      if (d.to_i)<0
+        negative << d
+        error=true
+      end
+      if (d.to_i<=1000)
       sum+=d.to_i
       end
+    end
+    if error
+      raise "negative numbers found: "+negative.join(",")
     end
     
     return sum.to_s+" from "+origin.to_s
@@ -50,6 +58,8 @@ puts "result should be 20, and is: "+add("//[;][,]\n4;5;6,5").to_s
 puts "result should be 15, and is: "+add("//[;]\n4;5;6").to_s
 puts "result should be 20, and is: "+add("//;\n4;5;6;5").to_s
 puts "result should be 1003, and is: "+add("1000,1001,3").to_s
-puts "result should be 12, and is: "+add("-3,4,-5,8").to_s
-puts "result should be 12, and is: "+add("//***\n-3***4***-5***8").to_s
-puts "result should be 12, and is: "+add("//[***][ee]\n-3***4ee-5***8").to_s
+puts "result should be 20, and is: "+add("3,4,5,8").to_s
+puts "result should be 20, and is: "+add("//***\n3***4***5***8").to_s
+puts "result should be 20, and is: "+add("//[***][ee]\n3***4ee5***8").to_s
+puts "next one should raise an error"
+puts "result should be an error, and is: "+add("-3,4,-5,8").to_s
