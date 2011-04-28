cb84d4fc8824126a2cc0572d23613d2ac2e59ac7
Silvio René <silvio.rene@gmail.com>
2011-04-28 21:21:11 +0800

    Initial commit

---
+++ remosu/string_calculator.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+
+def add(numbers):
+    if not numbers:
+        return 0
+    return sum(map(int, numbers.split(',')))
+
+if __name__ == '__main__':
+    assert add("") == 0
+    assert add("1") == 1
+    assert add("1,2") == 3
+    assert add("1,2,3") == 6
