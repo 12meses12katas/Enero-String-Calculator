a0f94872aeebd072163fc4d5f30b54072c4df13a
Silvio René <silvio.rene@gmail.com>
2011-04-28 21:32:29 +0800

    support different delimiters

--- remosu/string_calculator.py
+++ remosu/string_calculator.py
@@ -4,8 +4,12 @@
 def add(numbers):
     if not numbers:
         return 0
-    numbers = numbers.replace("\n", ",")
-    return sum(map(int, numbers.split(',')))
+    delimiter = ','
+    if numbers.startswith('//'):
+        delimiter, numbers = numbers.split('\n', 1)
+        delimiter = delimiter[2:]
+    numbers = numbers.replace("\n", delimiter)
+    return sum(map(int, numbers.split(delimiter)))
 
 if __name__ == '__main__':
     assert add("") == 0
@@ -13,3 +17,4 @@ if __name__ == '__main__':
     assert add("1,2") == 3
     assert add("1,2,3") == 6
     assert add("1\n2,3") == 6
+    assert add("//;\n1;2") == 3
