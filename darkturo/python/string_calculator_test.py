0d390298cf3898a2994d41fe66821d982225c121
Arturo Escudero <darkturo@gmail.com>
2011-05-22 08:22:03 +0800

    now

--- darkturo/python/string_calculator_test.py
+++ darkturo/python/string_calculator_test.py
@@ -1,1 +1,18 @@
-import 
+import unittest
+from string_calculator import StringCalculator
+
+class StringCalculatorTest(unittest.TestCase):
+    def test_call_add_with_empty_string(self):
+        calc = StringCalculator()
+        self.assertEqual(0, calc.add(""))
+
+    def test_call_add_with_one_number(self):
+        calc = StringCalculator()
+        self.assertEqual(1, calc.add("1"))
+
+    def test_call_add_with_two_numbers(self):
+        calc = StringCalculator()
+        self.assertEqual(3, calc.add("1,2"))
+
+if (__name__ == "__main__"):
+    unittest.main()
