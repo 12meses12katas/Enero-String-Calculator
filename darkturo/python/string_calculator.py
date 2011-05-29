f0805161e966480394fc224927ab271f3a3143a5
Arturo Escudero <darkturo@gmail.com>
2011-05-29 23:21:23 +0200

    Finito. Although a good refactoring may be done

--- darkturo/python/string_calculator.py
+++ darkturo/python/string_calculator.py
@@ -1,7 +1,7 @@
 import re
 
 class StringCalculator(object):
-    def __init__(self, delimiters = [","]):
+    def __init__(self, delimiters = [",", "\n"]):
         self.stringToParse = ""
         self.delimiters = delimiters
         self.numbers = []
@@ -24,7 +24,18 @@ class StringCalculator(object):
         self.numbers = self.convert2Numbers(chopped)
 
     def getExtraDelimiters(self):
-        return []
+        new_delimiters = []
+        if (self.stringToParse.find("//") != -1):
+            topIndex = self.stringToParse.find("\n")
+            subStringToParse = self.stringToParse[:topIndex]
+            self.stringToParse = self.stringToParse[topIndex + 1:]
+            if ((subStringToParse.find("[") != -1) and (subStringToParse.find("]"))):
+                subStringToParse = subStringToParse[3:]
+		subStringToParse = subStringToParse[:-1] 
+                new_delimiters = re.split("\]\[", subStringToParse)
+            else:
+                new_delimiters = [subStringToParse[2:]]
+        return new_delimiters
 
     def chopWith(self, delim, stringList):
         newList = []
@@ -33,10 +44,29 @@ class StringCalculator(object):
         return newList
 
     def _chopWith(self, delim, chunk):
+        chopped_with_delim = []        
         if (len(chunk) > 0):
-            return re.split(delim, chunk)
-        else:
-            return []
+            index = 0
+            while(chunk.find(delim) != -1):
+                index = chunk.find(delim)
+                chopped_with_delim.append(chunk[:index])
+                chunk = chunk[index + len(delim):]
+            chopped_with_delim.append(chunk)
+        return chopped_with_delim
 
     def convert2Numbers(self, theList):
-        return map(lambda x: int(x), theList)
+        numbers = []
+        negatives = []
+        for strNumber in (theList):
+            num = int(strNumber)
+            if (num < 0):
+                negatives.append(num)
+            elif (num < 1000): 
+                numbers.append(num)
+
+	if (len(negatives) > 0):
+            msg = "negatives not allowed: %s" % ",".join(map(lambda x: "%s"%x,
+                                                             negatives))
+            raise Exception(msg)
+
+        return numbers
