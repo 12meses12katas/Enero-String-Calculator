d9b6ddaada1b187e1222265b3563ddee45175e53
Arturo Escudero <darkturo@gmail.com>
2011-05-30 06:22:48 +0800

    Last update for this excercise in python. I have to read more about refactoring under a object oriented approach ... If I have time, I will make again this excercise in haskell

--- darkturo/python/string_calculator.py
+++ darkturo/python/string_calculator.py
@@ -3,39 +3,56 @@ import re
 class StringCalculator(object):
     def __init__(self, delimiters = [",", "\n"]):
         self.stringToParse = ""
-        self.delimiters = delimiters
-        self.numbers = []
+        self.initialDelimiters = delimiters
 
     def add(self, theString):
-        self.parseNumbersFromString(theString)
-        return reduce(lambda x, y: x + y, self.numbers, 0)
+        return reduce(lambda x, y: x + y, 
+                      self.retrieveNumbersFromString(theString), 0)
 
-    def parseNumbersFromString(self, stringWithNumbers):
-        self.numbers = []
+    def retrieveNumbersFromString(self, stringWithNumbers):
         self.stringToParse = stringWithNumbers
+        return self.convert2Numbers(self.getStringNumberList(
+                                       self.getDelimiters()))
 
-        delimiters = self.getExtraDelimiters()
-        delimiters.extend(self.delimiters)
-
+    def getStringNumberList(self, delimiters):
         chopped = [self.stringToParse]
         for delim in delimiters:
             chopped = self.chopWith(delim, chopped)
+	return chopped
 
-        self.numbers = self.convert2Numbers(chopped)
+    def getDelimiters(self):
+        delimiters = self.getExtraDelimiters()
+        delimiters.extend(self.initialDelimiters)
+        return delimiters
 
     def getExtraDelimiters(self):
         new_delimiters = []
-        if (self.stringToParse.find("//") != -1):
-            topIndex = self.stringToParse.find("\n")
-            subStringToParse = self.stringToParse[:topIndex]
-            self.stringToParse = self.stringToParse[topIndex + 1:]
-            if ((subStringToParse.find("[") != -1) and (subStringToParse.find("]"))):
-                subStringToParse = subStringToParse[3:]
-		subStringToParse = subStringToParse[:-1] 
-                new_delimiters = re.split("\]\[", subStringToParse)
+        if (self.newDelimitersDefined()):
+            subStringToParse = self.retrieveDelimitersStringDefinition()
+            if (self.isEnclosedByBrackets(subStringToParse)):
+                new_delimiters = re.split("\]\[", self.removeFirstAndLastChar\
+                                                           (subStringToParse))
             else:
-                new_delimiters = [subStringToParse[2:]]
+                new_delimiters = [subStringToParse]
         return new_delimiters
+
+    def newDelimitersDefined(self):
+        return (self.stringToParse.find("//") != -1)
+
+    def retrieveDelimitersStringDefinition(self):
+        topIndex = self.stringToParse.find("\n")
+        delimitersDef = self.stringToParse[:topIndex]
+        delimitersDef = delimitersDef[2:]
+        self.stringToParse = self.stringToParse[topIndex + 1:]
+        return delimitersDef
+
+    def isEnclosedByBrackets(self, substr):
+        return ((substr.find("[") != -1) and (substr.find("]") != -1))
+
+    def removeFirstAndLastChar(self, substr):
+        substr = substr[1:]
+        substr = substr[:-1] 
+        return substr
 
     def chopWith(self, delim, stringList):
         newList = []
@@ -64,7 +81,7 @@ class StringCalculator(object):
             elif (num < 1000): 
                 numbers.append(num)
 
-	if (len(negatives) > 0):
+        if (len(negatives) > 0):
             msg = "negatives not allowed: %s" % ",".join(map(lambda x: "%s"%x,
                                                              negatives))
             raise Exception(msg)
