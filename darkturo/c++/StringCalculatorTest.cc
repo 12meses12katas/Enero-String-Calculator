ba53fb52798b313ac0e7a76581040e605d828dfd
Arturo Escudero <darkturo@gmail.com>
2011-06-04 08:48:26 +0800

    some little changes on the unittest side

--- darkturo/c++/StringCalculatorTest.cc
+++ darkturo/c++/StringCalculatorTest.cc
@@ -3,8 +3,12 @@
 #include <cppunit/TestAssert.h>
 #include <cppunit/TestFixture.h>
 #include <cppunit/TextTestRunner.h>
+#include <cppunit/extensions/HelperMacros.h>
 
 class StringCalculatorTest : public CppUnit::TestFixture {
+    CPPUNIT_TEST_SUITE(StringCalculatorTest);
+    CPPUNIT_TEST(test_add_with_empty_string);
+    CPPUNIT_TEST_SUITE_END();
     private:
         StringCalculator * calc;
     public:
@@ -19,12 +23,17 @@ class StringCalculatorTest : public CppUnit::TestFixture {
     void test_add_with_empty_string() {
         CPPUNIT_ASSERT_EQUAL( 0, calc->add(""));
     }
+
+    void test_add_with_one_number() {
+        CPPUNIT_ASSERT_EQUAL( 1, calc->add("1"));
+    }
 };
+
 
 int main(int argc, char **argv) {
     CppUnit::TextTestRunner runner;
-    runner.addTest( new CppUnit::TestCaller<StringCalculatorTest>(
-                        "test_add_with_empty_string",
-                        &StringCalculatorTest::test_add_with_empty_string));
+
+    CPPUNIT_TEST_SUITE_REGISTRATION(StringCalculatorTest);
+    runner.addTest( CppUnit::TestFactoryRegistry::getRegistry().makeTest() );
     runner.run();
 }
