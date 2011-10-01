96c038784af12ed7271a90675522cf6cc8bd64d9
Iker Celorrio <i.celorrio@gmail.com>
2011-10-01 11:29:31 +0200

    Refactor After Test 2

--- ikercelorrio/java/StringCalculator/src/StringCalculator.java
+++ ikercelorrio/java/StringCalculator/src/StringCalculator.java
@@ -13,14 +13,11 @@ public class StringCalculator {
         if (numbers.isEmpty()) 
             return 0;
         
-        if (numbers.indexOf(',') == -1)
-            return Integer.parseInt(numbers);
-        
         String[] numArray = numbers.split(",");
-        int sum, max, i;
-        for ( sum = 0, i = 0, max = numArray.length ; i < max ; ++i)
+        int sum = 0;       
+        for ( String num : numArray)
         {
-            sum += Integer.parseInt(numArray[i]);
+            sum += Integer.parseInt(num);
         }
         return sum;
     }
