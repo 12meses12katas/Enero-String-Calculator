9cea84e3bd6e17f33b18c16de56c259eea452417
Sebastian Ortega <sortega@tid.es>
2011-10-01 18:03:39 +0200

    Regexes

--- sortega/haskell/calculator.hs
+++ sortega/haskell/calculator.hs
@@ -1,22 +1,39 @@
 module Calculator ( add ) where
 
-import Data.List.Split ( splitOneOf )
+import Text.Regex.Posix
+import Text.Regex ( mkRegex, splitRegex )
 
 add :: String -> Maybe Int
-add input = addDelim delimiter numbers
-    where (delimiter, numbers) = parseDelimiter input
+add input = addDelim delims numbers
+    where (delims, numbers) = parseDelimiters input
 
-addDelim :: Char -> String -> Maybe Int
+addDelim :: [String] -> String -> Maybe Int
 addDelim _ [] = Just 0
-addDelim delim input = 
+addDelim delims input = 
     case negatives of
-	[] -> Just (sum numbers)
+	[] -> Just (sum valid_numbers)
 	_  -> Nothing
-    where delims = delim : "\n"
-	  numbers = filter ((>=) 1000) $ map read $ splitOneOf delims input
+    where numbers = map read $ splitRegex regex input
+	  valid_numbers = filter ((>=) 1000) numbers
 	  negatives = filter ((>=) 0) numbers
+          regex = mkRegex $ foldl (\accum delim -> accum ++ '|' : delim)
+		            "\n" delims
 
-parseDelimiter :: String -> (Char, String)
-parseDelimiter input = case input of
-    '/':'/':delim:'\n':numbers -> (delim, numbers)
-    _ -> (',', input)
+parseDelimiters :: String -> ([String], String)
+parseDelimiters input = (delims, numbers)
+    where (_, delimSpec, numbers) = input =~ "^//([^\\n]*)\\n" :: (String, String, String)
+	  delims = case delimSpec of
+	    '/':'/':rest -> splitDelims rest
+	    _ -> [","]
+
+splitDelims :: String -> [String]
+splitDelims [] = []
+splitDelims input
+    | delim == [] = splitDelims rest
+    | otherwise   = delim : splitDelims rest
+    where (delim, _, rest) = input =~ "\\[|\\]+" :: (String, String, String)
+		
+escape :: Char -> String
+escape c | c `elem` regexChars = '\\' : [c]
+         | otherwise = [c]
+    where regexChars = "\\+*?()^$.{}]|["
