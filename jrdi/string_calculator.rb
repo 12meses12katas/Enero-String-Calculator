852fe243ad73b79e23d765aebf6de7e7e8c89683
Jordi Villar <jrdi.villar@gmail.com>
2011-10-30 00:39:13 +0200

    first step on ruby language

---
+++ jrdi/string_calculator.rb
@@ -0,0 +1,23 @@
+require 'rspec'
+
+class StringCalculator
+  
+  def self.add(string)
+    string.split(',').map(&:to_i).inject(0) {|total, number| total += number }
+  end
+  
+end
+
+describe 'StringCalculator.add' do
+  it "should return 0 for ''" do
+    StringCalculator.add('').should == 0
+  end
+  
+  it "should return 2 for '2'" do
+    StringCalculator.add('2').should == 2
+  end
+  
+  it "should return 5 for '2,3'" do
+    StringCalculator.add('2,3').should == 5
+  end
+end
